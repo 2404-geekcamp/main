acfee39bbe59b29ca3fe9bb06aa7a682e6d47fbb
ulxsth <ulxsth@gmail.com>
2024-04-24 08:20:24 +0900

    upd: デモ削除

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,8 @@
-import React, { useEffect,useState } from 'react';
 import './App.css';
-import axios from 'axios';
 
 const App = () => {
-  const [users, setUsers] = useState([])
-
-  useEffect(() => {
-    const fetchData = async() => {
-      const url = "/users";
-      try {
-        const response = await axios.get(url);
-        setUsers(response.data);
-      } catch(error) {
-        console.error('Error fetching data:', error);
-      }
-    }
-
-    fetchData();
-  }, []);
-
   return (
-    <div classname="App">
-      <h1>Users</h1>
-      {users.map((user,index) =>
-        <div key={index}>{user.name}</div>
-      )}
-    </div>
+    <div className="App"></div>
   );
 }
 
