7c781f0421868d26cf4b49f6540242e3910491ff
ulxsth <ulxsth@gmail.com>
2024-04-24 08:49:52 +0900

    upd: 静的ファイルの指定位置をclient内のpublicに変更

--- express.js
+++ express.js
@@ -12,7 +12,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// 静的ファイルの位置をReact側のビルドファイルに
+app.use(express.static(path.join(__dirname, 'client/public')));
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
 
 app.use('/', indexRouter);
 
