902dad39b3db5ca99e65ac8b4c191888c8dcf5dd
kodai <kodai030513@gmail.com>
2024-04-24 12:25:04 +0900

    挿入が成功しているかを戻り値で返すように処理を追加

--- app/models/UserModel.js
+++ app/models/UserModel.js
@@ -12,9 +12,10 @@ class UserModel {
     *  @params name string ユーザー名
     *  @params email string ユーザーが入力したメールアドレス
     *  @params password_hash string ユーザーが入力したパスワードをハッシュ化したもの
+    *  @returns bool DBへの挿入が成功したか否かをbool値で返す。trueの場合は成功、falseの場合は失敗
     */
     async insert(name, email, password_hash, experience_option_id, stance_option_id) {
-        await this.#connection
+        const { error } = await this.#connection
             .from('users')
             .insert({
                 'name':                 name,
@@ -23,6 +24,8 @@ class UserModel {
                 'experience_option_id': experience_option_id,
                 'stance_option_id':     stance_option_id
             });
+        
+        return error ? false : true;
     }
 }
 
