e6d8700d70e22c78ef9a6e14c2dd75a16e35baa8
yotu <114195789+ulxsth@users.noreply.github.com>
2024-04-25 09:30:01 +0900

    upd: ファイル名を大文字に

--- client/src/pages/InviteMessages.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const InviteMessages = () => {
-  return (
-    <h1>Invite Messages</h1>
-  )
-}
-
-export default InviteMessages;
--- client/src/pages/auth/initProfile.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const InitProfile = () => {
-  return (
-    <h1>Init profile</h1>
-  )
-}
-
-export default InitProfile;
