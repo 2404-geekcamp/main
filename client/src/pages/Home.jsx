e08db3bf46911db0f9f435a11acc12133b79a2ef
yotu <114195789+ulxsth@users.noreply.github.com>
2024-04-25 10:37:04 +0900

    upd: 例

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 const home = () => {
+  let [message, setMessage] = React.useState('')
+
+  useEffect(() => {
+    const axiosInstance = axios.create({
+    withCredentials: false,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+    const res = axiosInstance.get(`http://localhost:3001/feeds`, {
+    }).then((res) => {
+      console.log(res.data);
+      setMessage(res.data.message)
+    }).catch((error) => {
+      console.error(error);
+    });
+  }, [])
+
   return (
-    <h1>Home</h1>
+    <h1>{ message }</h1>
   )
 }
 
--- server/config/routes/index.js
+++ server/config/routes/index.js
@@ -1,19 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.redirect('/login');
+router.get('/feeds', function(req, res, next) {
+  res.json({ message: 'Hello, world!' });
 });
-
-// auth
-router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Login' });
-});
-
-router.get('/register', function (req, res, next) {
-  res.render('register', { title: 'Register' });
-});
-
-
 
 module.exports = router;
