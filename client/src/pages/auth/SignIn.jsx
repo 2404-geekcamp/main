f088acb2a3c40cba48604b039224a4dec9f44718
PoPodada <goito.722@icloud.com>
2024-04-26 17:46:52 +0900

    signin ページの要素を作成した

--- client/src/pages/auth/SignIn.jsx
+++ client/src/pages/auth/SignIn.jsx
@@ -6,16 +6,16 @@ const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   return (
-    <div className='wrapper'>
-      <h1>ログイン</h1>
-      <form action="">
+    <div className=''>
+      <h1 className='text-center text-3xl font-bold my-10'>ログイン</h1>
+      <form action="" className='flex flex-col mx-auto w-[300px]'>
         <label htmlFor="email">メールアドレス</label>
-        <input type="email" name="email" id="email" onChange={(e)=>setEmail(e.target.value)} value={email}/>
+        <input type="email" name="email" id="email" className='mt-[5px] mb-[20px] h-[40px] border-2' onChange={(e)=>setEmail(e.target.value)} value={email}/>
         <label htmlFor="password">パスワード</label>
-        <input type="password" name="password" id="password" onChange={(e)=>setPassword(e.target.value)} value={password}/>
-        <button type="submit">ログイン</button>
+        <input type="password" name="password" id="password" className='mt-[5px] mb-[20px] h-[40px] border-2' onChange={(e)=>setPassword(e.target.value)} value={password}/>
+        <button type="submit" className='w-[200px] bg-black text-white mx-auto h-[40px]'>ログイン</button>
       </form>
-      <Link to="/signup" className='toLogin'>新規登録画面へ</Link>
+      <Link to="/signup" className='block text-center mt-[40px] text-blue-500'>新規登録画面へ</Link>
     </div>
   )
 };
