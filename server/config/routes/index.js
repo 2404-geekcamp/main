c54adf96b2ccc6f7c2ac5cd9e982fadb04cc9299
yotu <114195789+ulxsth@users.noreply.github.com>
2024-04-26 10:00:02 +0900

    upd: POST /search

--- server/config/routes/index.js
+++ server/config/routes/index.js
@@ -3,7 +3,12 @@ var UserController = require('../../controllers/UserController');
 var router = express.Router();
 
 
-module.exports = function(db) {
-  
+module.exports = function (db) {
+  router.post('/search', async function (req, res, next) {
+    const userController = new UserController(db);
+    const users = await userController.search(req.body.skill_ids, req.body.experience_id, req.body.stance_id);
+    res.json(users);
+  })
+
   return router;
 }
