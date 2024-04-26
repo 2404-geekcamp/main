9eccb94eb9b8e01cf345011e7f3ec96e01165241
PoPodada <goito.722@icloud.com>
2024-04-26 17:31:15 +0900

    css修正完了

--- client/src/pages/auth/SignUp.jsx
+++ client/src/pages/auth/SignUp.jsx
@@ -7,17 +7,17 @@ const SignUp = () => {
   const [password, setPassword] = useState('');
   return (
     <div className=''>
-      <h1 className='text-center'>新規登録</h1>
+      <h1 className='text-center text-3xl font-bold my-10'>新規登録</h1>
       <form action="" className='flex flex-col mx-auto w-[300px]'>
         <label htmlFor="username">ユーザー名</label>
-        <input type="text" name="username" id="username" className='mt-[5px] mb-[20px] h-[40px]'/>
+        <input type="text" name="username" id="username" className='mt-[5px] mb-[20px] h-[40px] border-2'/>
         <label htmlFor="email">メールアドレス</label>
-        <input type="email" name="email" id="email" className='mt-[5px] mb-[20px] h-[40px]'/>
+        <input type="email" name="email" id="email" className='mt-[5px] mb-[20px] h-[40px] border-2'/>
         <label htmlFor="password">パスワード</label>
-        <input type="password" name="password" id="password" className='mt-[5px] mb-[20px] h-[40px]'/>
+        <input type="password" name="password" id="password" className='mt-[5px] mb-[20px] h-[40px] border-2'/>
         <button type="submit" className='w-[200px] bg-black text-white mx-auto h-[40px]'>登録</button>
       </form>
-      <Link to="/signin" className='block text-center mt-[40px]'>ログイン画面へ</Link>
+      <Link to="/signin" className='block text-center mt-[40px] text-blue-500'>ログイン画面へ</Link>
     </div>
   )
 }
