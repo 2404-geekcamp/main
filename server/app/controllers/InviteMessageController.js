9b97988e80e527f4083554fab1ddf6182bd8caa4
ulxsth <ulxsth@gmail.com>
2024-04-26 22:43:29 +0900

    upd: implement

--- server/app/controllers/InviteMessageController.js
+++ server/app/controllers/InviteMessageController.js
@@ -1,6 +1,20 @@
+const InviteMessageModel = require("../models/InviteMessageModel");
+
 module.exports = class InviteMessageController {
   #db = null;
   constructor(db) {
     this.#db = db;
+  }
+
+  /**
+   * 招待メッセージを新しく作成する。
+   * @param to_user_id {number} 招待メッセージの送信先ユーザーID
+   * @param from_user_id {number} 招待メッセージの送信元ユーザーID
+   * @param content {string} 招待メッセージの内容
+   * @return number 新しく作成された招待メッセージのID
+   */
+  async create(to_user_id, from_user_id, content) {
+    const model = new InviteMessageModel(this.#db);
+    return await model.create(to_user_id, from_user_id, content);
   }
 }
--- server/app/models/InviteMessageModel.js
+++ server/app/models/InviteMessageModel.js
@@ -3,4 +3,28 @@ module.exports = class InviteMessageModel {
   constructor(db) {
     this.#db = db;
   }
+
+  /**
+   * 招待メッセージを新しく作成する。
+   * @param to_user_id {number} 招待メッセージの送信先ユーザーID
+   * @param from_user_id {number} 招待メッセージの送信元ユーザーID
+   * @param content {string} 招待メッセージの内容
+   * @return number 新しく作成された招待メッセージのID
+   */
+  async create(to_user_id, from_user_id, content) {
+    const { data, error } = await this.#db.connect()
+      .from('invite_messages')
+      .insert({
+        'to_user_id': to_user_id,
+        'from_user_id': from_user_id,
+        'content': content
+      })
+      .select("id");
+    if (error) {
+      console.error(error);
+      return null;
+    }
+
+    return data?.id;
+  }
 }
