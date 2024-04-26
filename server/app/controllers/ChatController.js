e33f48b7cb5da9a4656bf50a9de3cc1be1e80335
yotu <114195789+ulxsth@users.noreply.github.com>
2024-04-26 13:52:54 +0900

    upd: controller.create

--- server/app/controllers/ChatController.js
+++ server/app/controllers/ChatController.js
@@ -1,6 +1,17 @@
+const ChatModel = require("../models/ChatModel");
+
 module.exports = class ChatController {
   #db = null;
   constructor(db) {
     this.#db = db;
+  }
+
+  /**
+   * チャットを新しく作成する。
+   * @return boolean 成功したか
+   */
+  async create() {
+    const ChatModel = new ChatModel(this.#db);
+    return await ChatModel.create();
   }
 }
