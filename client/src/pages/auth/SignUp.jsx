d549d13266b698237c89a2e00dc189d94ba1d038
PoPodada <goito.722@icloud.com>
2024-04-26 14:45:47 +0900

    デザイン通りの要素のみを追加

--- client/src/pages/auth/SignUp.jsx
+++ client/src/pages/auth/SignUp.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import '../../styles/SignUp.css'
 
 const SignUp = () => {
   return (
-    <h1>Sign Up</h1>
+    <div className='wrapper'>
+      <h1>新規登録</h1>
+      <form action="">
+        <label htmlFor="username">ユーザー名</label>
+        <input type="text" name="username" id="username" />
+        <label htmlFor="email">メールアドレス</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password">パスワード</label>
+        <input type="password" name="password" id="password" />
+        <button type="submit">登録</button>
+      </form>
+      <a href="">ログイン画面へ</a>
+    </div>
   )
 }
 
