846cab6bcde83d23ebd76f63d23e1b4389c3923f
yotu <114195789+ulxsth@users.noreply.github.com>
2024-04-26 09:55:30 +0900

    del: テストコード

--- server/config/routes/index.js
+++ server/config/routes/index.js
@@ -4,17 +4,6 @@ var router = express.Router();
 
 
 module.exports = function(db) {
-  router.get('/', async function(req, res, next) {
-    const userController = new UserController(db);
-    req.body.email = "Tanaka";
-    req.body.password = "Tanaka";
-    await userController.login(req);
-    res.json(req.session);
-  });
-
-  router.get('/feeds', function(req, res, next) {
-    res.json({ message: 'Hello, world!' });
-  });
-
+  
   return router;
 }
