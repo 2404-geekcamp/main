f5bcb60268a70d547752571164156fe383728881
PoPodada <goito.722@icloud.com>
2024-04-26 15:49:57 +0900

    signinページの要素を作成

--- client/src/pages/auth/SignIn.jsx
+++ client/src/pages/auth/SignIn.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <div>
-      <h1>Sign In</h1>
+    <div className='wrapper'>
+      <h1>ログイン</h1>
+      <form action="">
+        <label htmlFor="email">メールアドレス</label>
+        <input type="email" name="email" id="email" onChange={(e)=>setEmail(e.target.value)} value={email}/>
+        <label htmlFor="password">パスワード</label>
+        <input type="password" name="password" id="password" onChange={(e)=>setPassword(e.target.value)} value={password}/>
+        <button type="submit">ログイン</button>
+      </form>
+      <Link to="/signup" className='toLogin'>新規登録画面へ</Link>
     </div>
-  );
+  )
 };
 
 export default SignIn;
