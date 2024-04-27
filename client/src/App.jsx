39638bc21c8d5060f788e7546deda3b5e49b3086
go <goito.722@icloud.com>
2024-04-27 14:25:05 +0900

    テスト用のapp.jsx の中の要素を削除

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import Result from './pages/search/Result'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <><Result/></>
+    <></>
   )
 }
 
