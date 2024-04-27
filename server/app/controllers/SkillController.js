b059c3513ec2ffbdcbdc38f9674b01956e611dcf
ulxsth <ulxsth@gmail.com>
2024-04-27 14:28:45 +0900

    upd: implement

--- server/app/controllers/SkillController.js
+++ server/app/controllers/SkillController.js
@@ -1,6 +1,17 @@
+const SkillModel = require("../models/SkillModel");
+
 module.exports = class SkillController {
   #db = null;
   constructor(db) {
     this.#db = db;
+  }
+
+  /**
+   * 技術の選択肢を取得する。
+   * @return {Object[]} 技術の選択肢
+   */
+  async fetchAll() {
+    const model = new SkillModel(this.#db);
+    return await model.fetchAll();
   }
 }
--- server/app/models/SkillModel.js
+++ server/app/models/SkillModel.js
@@ -3,4 +3,14 @@ module.exports = class SkillModel {
   constructor(db) {
     this.#db = db;
   }
+
+  /**
+   * 技術の選択肢を取得する。
+   * @return {Object[]} 技術の選択肢
+   */
+  async fetchAll() {
+    return await this.#db.connect()
+      .from('skills')
+      .select();
+  }
 }
