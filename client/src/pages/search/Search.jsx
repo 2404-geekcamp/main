41740690dbe1c8f02ae3508e42bf4b6fcdfbde43
ulxsth <ulxsth@gmail.com>
2024-04-27 15:33:20 +0900

    upd: implement fetch

--- client/src/pages/search/Search.jsx
+++ client/src/pages/search/Search.jsx
@@ -1,10 +1,36 @@
+import axios from 'axios';
 import React from 'react'
+import { useEffect, useState } from 'react'
+
+const apiUrl = import.meta.env.VITE_API_SERVER_URL;
 
 const Search = () => {
-  return (
-    <div>
+  const [skills, setSkills] = useState([]);
+  useEffect(() => {
+    axios.get(apiUrl + "/skills")
+      .then((res) => {
+        setSkills(res.data);
+      })
+  }, []);
+  console.log(skills)
 
-    </div>
+  return (
+    <>
+      <form action="#" method="post">
+        <div className="skills">
+          {
+            skills.map((skill) => {
+              return (
+                <div key={skill.id} className="skill">
+                  <input type="checkbox" name="skill" value={skill.id} id={skill.id} />
+                  <label htmlFor={skill.id}>{skill.name}</label>
+                </div>
+              )
+            })
+          }
+        </div>
+      </form>
+    </>
   )
 }
 
