a6ec2d1bd54284c9cabc0ec816fb711776fed47b
go <goito.722@icloud.com>
2024-04-27 15:31:20 +0900

    ボタンを押したときに色の変化と状態管理ができるようにしました

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-
+import InitProfile from './pages/auth/InitProfile'
+import Button from './Button'
 
 function App() {
   const [count, setCount] = useState(0)
--- client/src/pages/auth/InitProfile.jsx
+++ client/src/pages/auth/InitProfile.jsx
@@ -1,40 +1,39 @@
 import React from 'react'
+import Button from '../../Button';
 
 const InitProfile = () => {
   return (
-    <div className='mx-auto max-w-[900px] h-screen pt-32 mx-10 px-10'>
+    <div className='mx-auto max-w-[900px] h-screen pt-32  px-8' >
+
       <h1 className='text-center text-3xl font-bold my-10'>プロフィール設定</h1>
+
       <div className='mx-auto'>
         <h2 className='text-xl font-bold mt-10'>技術</h2>
         <div className='mt-4 flex flex-wrap'>
-          <p className='m-1 p-2 bg-slate-200 rounded-full'>サンプル</p>
-          <p className='m-1 p-2 bg-slate-200 rounded-full'>フロントエンド</p>
-          <p className='m-1 p-2 bg-slate-200 rounded-full'>バックエンド</p>
-          <p className='m-1 p-2 bg-slate-200 rounded-full'>インフラ</p>
-          <p className='m-1 p-2 bg-slate-200 rounded-full'>Figma</p>
+          <Button text={"フロントエンド"} id={"1"}/>
+          <Button text={"バックエンド"} id={"2"}/>
+          <Button text={"インフラ"} id={"3"}/>
         </div>
       </div>
+
       <div>
         <h2 className='text-xl font-bold mt-10'>ハッカソン出場経験</h2>
         <div className='mt-4 flex flex-wrap'>
-          <p className='m-1 p-2 bg-slate-200 rounded-full'>0回</p>
-          <p className='m-1 p-2 bg-slate-200 rounded-full'>1回~3回</p>
-          <p className='m-1 p-2 bg-slate-200 rounded-full'>4回~9回</p>
-          <p className='m-1 p-2 bg-slate-200 rounded-full'>10回以上</p>
+          <Button text={"0回"} id={"4"}/>
+          <Button text={"1回~3回"} id={"5"}/>
         </div>
       </div>
       <div>
         <h2 className='text-xl font-bold mt-10'>ハッカソンへのスタンス</h2>
         <div className='mt-4 flex flex-wrap'>
-          <p className='m-1 p-2 bg-slate-200 rounded-full'>とりあえず出てみたい</p>
-          <p className='m-1 p-2 bg-slate-200 rounded-full'>楽しみたい</p>
-          <p className='m-1 p-2 bg-slate-200 rounded-full'>賞を狙いたい</p>
-          <p className='m-1 p-2 bg-slate-200 rounded-full'>新しい技術を学びたい</p>
-          
+          <Button text={"とりあえず楽しみたい"} id={"6"}/>
+          <Button text={"入賞希望"} id={"7"}/>
+          <Button text={"がんばる"} id={"8"}/>
+
         </div>
       </div>
-      
-      
+
+
     </div>
   )
 }
