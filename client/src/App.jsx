debb9ebf448461907ffebfe7b0d8c968f2e3e1e8
go <goito.722@icloud.com>
2024-04-27 13:09:01 +0900

    テストのために書いていたapp.jsx内のタグを削除

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import UserId from './pages/UserId.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <UserId />
-    </>
+    <></>
   )
 }
 
