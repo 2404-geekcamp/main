957516cf607f5e60b0e738fbd1f98f7d8c65c114
Huyu2239 <huyu2239@gmail.com>
2024-04-27 13:32:52 +0900

    add: controller.fetch

--- server/app/controllers/ChatListController.js
+++ server/app/controllers/ChatListController.js
@@ -1,6 +1,18 @@
+const ChatListModel = require("../models/ChatListModel");
+
 module.exports = class ChatListController {
 	#db = null;
 	constructor(db) {
 		this.#db = db;
 	}
+
+  /**
+   * ユーザーidを受け取り、ユーザーが持つチャットのidリストを返す。
+	 * @param user_id number ユーザーID
+   * @return Object[] チャットルーム情報の配列
+   */
+  async fetch(user_id) {
+    const model = new ChatListModel(this.#db);
+    return await model.fetch(user_id);
+  }
 }
