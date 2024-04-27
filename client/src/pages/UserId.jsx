6c0dae72583c66af355515afd581c5a064244cae
go <goito.722@icloud.com>
2024-04-27 13:02:45 +0900

    プロフィール画面の要素作成

--- client/src/pages/UserId.jsx
+++ client/src/pages/UserId.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
+import icon from '../samples/icon.png'
+
+
 
 const userId = () => {
   let { id } = useParams();
   return (
-    <h1>{ id }</h1>
+    <div className='mx-auto max-w-[900px] p-8 bg-slate-300	 mx-8 my-8 rounded-md'>
+      {/* <h1>{ id }</h1> */}
+      <div className='flex  '>
+        <img src={icon} alt="icon sample" style={{ width: '80px'}} />
+        <h2 className='text-2xl font-bold ml-4'>BIG GYOZA</h2>
+        
+      </div>
+      <div>
+          <p className='text-xl font-bold mt-10'>技術</p>
+          <div className='flex max-w-full flex-wrap w-4/5	'>
+          <p className='mx-6 my-2 bg-white p-1 rounded-lg'>Sample</p>
+          <p className='mx-6 my-2 bg-white p-1 rounded-lg'>Sample</p>
+          <p className='mx-6 my-2 bg-white p-1 rounded-lg'>Sample</p>
+          <p className='mx-6 my-2 bg-white p-1 rounded-lg'>Sample</p>
+          <p className='mx-6 my-2 bg-white p-1 rounded-lg'>Sample</p>
+          <p className='mx-6 my-2 bg-white p-1 rounded-lg'>Sample</p>
+          <p className='mx-6 my-2 bg-white p-1 rounded-lg'>Sample</p>
+          
+          
+          </div>
+          
+      </div>
+    </div>
+    
+
   )
 }
 
