bb157489234ed5b03c3b9cd32848e18f23be8d96
go <goito.722@icloud.com>
2024-04-27 22:46:51 +0900

    ホーム画面の要素を作成

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const home = () => {
   let [message, setMessage] = React.useState('')
@@ -21,7 +22,18 @@ const home = () => {
   }, [])
 
   return (
-    <h1>{ message }</h1>
+    <div className='max-w-[900px] mx-auto'>
+      <button className='bg-indigo-700 block w-[300px] mx-auto h-[100px] rounded-xl font-bold text-xl text-white mb-40'>
+        一緒にハッカソンに出る仲間を探す
+      </button>
+      <div className='mx-auto px-10'>
+        
+        <Link to="/chats" className='bg-indigo-700 block text-white h-[70px] rounded-xl font-bold text-xl w-full justify-center flex items-center my-10'>DM一覧</Link>
+        <Link to="/invites" className='bg-indigo-700 block text-white h-[70px] rounded-xl font-bold text-xl w-full justify-center flex items-center'>招待メッセージ一覧</Link>
+        
+      </div>
+    </div>
+    // <h1>{ message }</h1>
   )
 }
 
