8453b9f31434cb87fb21dbd11cdd6d545617cd34
ulxsth <ulxsth@gmail.com>
2024-04-27 10:15:58 +0900

    upd: implement

--- server/app/controllers/JoinController.js
+++ server/app/controllers/JoinController.js
@@ -1,6 +1,19 @@
+const JoinModel = require("../models/JoinModel");
+
 module.exports = class JoinController {
   #db = null;
   constructor(db) {
     this.#db = db;
+  }
+
+  /**
+   * チャットに新しくユーザーを追加する
+   * @param {string} chat_id チャットID
+   * @param {string} user_id ユーザーID
+   * @return number 追加した行のid
+   */
+  async join(chat_id, user_id) {
+    const model = new JoinModel(this.#db);
+    return await model.insert(chat_id, user_id);
   }
 }
