d1dd2f9d7b3e9d85533ea8039fc79d7e67c634e2
haru <hinagoro5656@gmail.com>
2024-04-28 15:09:52 +0900

    ホームのデザインを修正

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -29,13 +29,13 @@ const home = () => {
     <Header backPath={""} back={<img src={logo} className='w-20'/>} forWardPath={"/user/1"} forward={"マイページ"}/>
 
     <div className='max-w-[900px] mx-auto mt-20'>
-      <Link to="/search" className='bg-indigo-700 block w-[300px] mx-auto h-[100px] rounded-xl font-bold text-xl text-white mb-40 flex justify-center items-center'>
+      <Link to="/search" className='bg-indigo-700 text-center p-4 w-[300px] mx-auto h-[100px] rounded-lg font-bold text-xl text-white mb-40 flex justify-center items-center'>
          一緒にハッカソンに出る仲間を探す
       </Link>
       <div className='mx-auto px-10'>
         
-        <Link to="/chats" className='bg-indigo-700 block text-white h-[70px] rounded-xl font-bold text-xl w-full justify-center flex items-center my-10'>DM一覧</Link>
-        <Link to="/invites" className='bg-indigo-700 block text-white h-[70px] rounded-xl font-bold text-xl w-full justify-center flex items-center'>招待メッセージ一覧</Link>
+        <Link to="/chats" className='bg-indigo-700 text-white h-[70px] rounded-lg font-bold text-xl w-full justify-center flex items-center my-10'>DM一覧</Link>
+        <Link to="/invites" className='bg-indigo-700 text-white h-[70px] rounded-lg font-bold text-xl w-full justify-center flex items-center'>招待メッセージ一覧</Link>
         
       </div>
     </div>
