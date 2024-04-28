4f284ff83262c79c897e26f8fb36260eed349b3d
ulxsth <ulxsth@gmail.com>
2024-04-28 12:57:27 +0900

    fix

--- server/config/routes/index.js
+++ server/config/routes/index.js
@@ -14,8 +14,9 @@ const StanceOptionController = require('../../app/controllers/StanceOptionContro
 
 module.exports = function (db) {
   router.post('/search', async function (req, res, next) {
+    console.log(req.body);
     const userController = new UserController(db);
-    const users = await userController.search(req.body.skill_ids, req.body.experience_id, req.body.stance_id);
+    const users = await userController.search(req.body.skill_ids, req.body.experience, req.body.stance);
     res.json(users);
   });
 
