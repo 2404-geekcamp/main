aad7fc44c1ac8026d671be6661c9868284bc7e6e
Huyu2239 <huyu2239@gmail.com>
2024-04-28 11:45:55 +0900

    fix: plugin-basic-sslやめて証明書利用

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   server: {
-    host: true
+    host: true,
+    https: {
+      key: fs.readFileSync('../cert/localhost-key.pem'),
+      cert: fs.readFileSync('../cert/localhost.pem'),
+  },
   }
 })
