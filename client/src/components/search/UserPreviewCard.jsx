f3bc6889696ca91f48f026405952c13445d964e1
ulxsth <ulxsth@gmail.com>
2024-04-28 12:25:53 +0900

    upd: ユーザー画面に遷移するリンク

--- client/src/components/search/UserPreviewCard.jsx
+++ client/src/components/search/UserPreviewCard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 import UserSkillBadges from "./UserSkillBadges";
 
 const apiUrl = import.meta.env.VITE_API_SERVER_URL;
@@ -15,7 +16,9 @@ const UserPreviewCard = ({ user }) => {
   return (
     <div className="mx-10 px-10 border-2 rounded-lg border-black mb-2">
       <div className="flex justify-between pt-4">
-        <h3 className="font-bold text-xl">{user.name}</h3>
+        <h3 className="font-bold text-xl">
+          <Link to={`/user/${user.id}`}>{user.name}</Link>
+        </h3>
         <p className="text-white bg-indigo-700 px-2 rounded-xl ">2 matches</p>
       </div>
       <UserSkillBadges skills={skills} />
