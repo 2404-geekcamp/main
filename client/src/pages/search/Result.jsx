961a6b59b96ef3f07a29a98e2378b4d05e3312d1
go <goito.722@icloud.com>
2024-04-28 12:44:29 +0900

    loadingコンポーネントにした

--- client/src/pages/search/Result.jsx
+++ client/src/pages/search/Result.jsx
@@ -2,6 +2,7 @@ import React, { Suspense, useEffect } from 'react'
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import UserPreviewCard from '../../components/search/UserPreviewCard';
+import Loading from '../../components/Loading';
 
 const apiUrl = import.meta.env.VITE_API_SERVER_URL;
 
@@ -17,16 +18,11 @@ const Result = () => {
       .post(apiUrl + "/search", { skill_ids, experience, stance })
       .then((res) => {
         setUsers(res.data);
-        setLoading(false)
       });
   }, []);
   if(users.length === 0){
     return(
-      <div className='max-w-[900px] mx-auto h-screen'>
-        <h2 className="text-center text-3xl font-bold py-10">検索結果</h2>
-        <div className='flex justify-center items-center mt-[200px] font-bold text-xl'>検索中...</div>
-    </div>
-      
+      <Loading />
     )
   }
 
