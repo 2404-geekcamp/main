821fd163af612b9580e46cced0bf673649f3c88a
haru <hinagoro5656@gmail.com>
2024-04-28 13:44:33 +0900

    プロフィール設定をCheckButtonに変更

--- client/src/pages/auth/InitProfile.jsx
+++ client/src/pages/auth/InitProfile.jsx
@@ -1,41 +1,102 @@
-import React from 'react'
-import Button from '../../components/Button';
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import CheckButton from "../../components/CheckButton.jsx";
+
+const apiUrl = import.meta.env.VITE_API_SERVER_URL;
 
 const InitProfile = () => {
+  const [skills, setSkills] = useState([]);
+  useEffect(() => {
+    axios.get(apiUrl + "/skills").then((res) => {
+      setSkills(res.data.data);
+    });
+  }, []);
+
+  const [experiences, setExperiences] = useState([]);
+  useEffect(() => {
+    axios.get(apiUrl + "/experience_options").then((res) => {
+      setExperiences(res.data);
+    });
+  }, []);
+
+  const [stances, setStances] = useState([]);
+  useEffect(() => {
+    axios.get(apiUrl + "/stance_options").then((res) => {
+      setStances(res.data);
+    });
+  }, []);
+
   return (
-    <div className='mx-auto max-w-[900px] h-screen pt-32  px-8' >
-
-      <h1 className='text-center text-3xl font-bold my-10'>プロフィール設定</h1>
-
-      <div className='mx-auto'>
-        <h2 className='text-xl font-bold mt-10'>技術</h2>
-        <div className='mt-4 flex flex-wrap'>
-          <Button text={"フロントエンド"} id={"1"} />
-          <Button text={"バックエンド"} id={"2"} />
-          <Button text={"インフラ"} id={"3"} />
+    <div className="mx-auto max-w-[900px] h-screen pt-32  px-8">
+      <h1 className="text-center text-3xl font-bold my-10">プロフィール設定</h1>
+      <form action="" className="flex flex-col gap-8">
+        <div>
+          <div className="mb-4 flex gap-4 items-end">
+            <h2 className="text-2xl">技術</h2>
+            <span className="text-gray-500">複数選択可</span>
+          </div>
+          <div className="flex flex-wrap gap-x-2 gap-y-2">
+            {skills.map((skill) => {
+              return (
+                <CheckButton
+                  key={skill.id}
+                  type="checkbox"
+                  inputName="skills"
+                  value={skill.id}
+                  Name={skill.name}
+                ></CheckButton>
+              );
+            })}
+          </div>
         </div>
-      </div>
-
-      <div>
-        <h2 className='text-xl font-bold mt-10'>ハッカソン出場経験</h2>
-        <div className='mt-4 flex flex-wrap'>
-          <Button text={"0回"} id={"4"} />
-          <Button text={"1回~3回"} id={"5"} />
+        <div>
+          <div className="mb-4 flex gap-4 items-end">
+            <h2 className="text-2xl">経験</h2>
+            <span className="text-gray-500">単一選択</span>
+          </div>
+          <div className="flex flex-wrap gap-x-2 gap-y-2">
+            {experiences.map((experience) => {
+              return (
+                <CheckButton
+                  key={experience.id}
+                  type="radio"
+                  inputName="experience"
+                  value={experience.id}
+                  Name={experience.name}
+                ></CheckButton>
+              );
+            })}
+          </div>
         </div>
-      </div>
-      <div>
-        <h2 className='text-xl font-bold mt-10'>ハッカソンへのスタンス</h2>
-        <div className='mt-4 flex flex-wrap'>
-          <Button text={"とりあえず楽しみたい"} id={"6"} />
-          <Button text={"入賞希望"} id={"7"} />
-          <Button text={"がんばる"} id={"8"} />
-
+        <div>
+          <div className="mb-4 flex gap-4 items-end">
+            <h2 className="text-2xl">スタンス</h2>
+            <span className="text-gray-500">単一選択</span>
+          </div>
+          <div className="flex flex-wrap gap-x-2 gap-y-2">
+            {stances.map((stance) => {
+              return (
+                <CheckButton
+                  key={stance.id}
+                  type="radio"
+                  inputName="stance"
+                  value={stance.id}
+                  Name={stance.name}
+                ></CheckButton>
+              );
+            })}
+          </div>
         </div>
-      </div>
-
-
+        <button
+          type="submit"
+          className="py-2 px-6 mt-8 rounded-full border border-gray-700 hover:bg-gray-50"
+        >
+          設定
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default InitProfile;
