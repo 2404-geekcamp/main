a5bdada75888457b7a4a902483de1a7c6be896e7
ulxsth <ulxsth@gmail.com>
2024-04-28 14:06:36 +0900

    upd: inviteMessage fetch 成功

--- client/src/pages/InviteMessages.jsx
+++ client/src/pages/InviteMessages.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import InviteMessagePreview from "../components/InviteMessagePreview";
+import axios from "axios";
+
+const apiUrl = import.meta.env.VITE_API_SERVER_URL;
 
 const InviteMessages = () => {
+  const userId = 1;   // TODO: セッションに置きかえ
+  const [inviteMessages, setInviteMessages] = React.useState([]);
+  useEffect(() => {
+    axios.get(apiUrl + "/invite_messages/received/" + userId).then((res) => {
+      setInviteMessages(res.data);
+    });
+  }, []);
+
   return (
     <div className="max-w-[900px] mx-auto">
       <h1 className="text-center text-3xl font-bold py-10">招待メッセージ</h1>
       <div className="border-t">
-        <InviteMessagePreview />
+        {inviteMessages.map((inviteMessage) => (
+          <InviteMessagePreview
+            key={inviteMessage.id}
+            inviteMessage={inviteMessage}
+          />
+        ))}
       </div>
     </div>
   );
