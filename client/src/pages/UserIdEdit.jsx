1a7ec387dec52769f48a93d95a0d89884574401c
haru <hinagoro5656@gmail.com>
2024-04-28 14:05:40 +0900

    プロフィール編集画面をCheckButtonに変更

--- client/src/pages/UserIdEdit.jsx
+++ client/src/pages/UserIdEdit.jsx
@@ -1,61 +1,137 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import icon from '../samples/icon.png'
-import { useState } from 'react'
-import Button from '../components/Button'
-import { Header } from '../components/Header'
+import React from "react";
+import { useParams } from "react-router-dom";
+import icon from "../samples/icon.png";
+import { useState, useEffect } from "react";
+import { Header } from "../components/Header";
+import axios from "axios";
+import CheckButton from "../components/CheckButton.jsx";
+
+const apiUrl = import.meta.env.VITE_API_SERVER_URL;
 
 const userIdEdit = () => {
   let { id } = useParams();
   //usestateでもともとの値を設定するといった想定
-  const [userName, setUserName] = useState("Big Gyoza")
-  const [userProfile,setUserProfile] = useState('でたい') 
+  const [userName, setUserName] = useState("Big Gyoza");
+  const [userProfile, setUserProfile] = useState("でたい");
+
+  const [skills, setSkills] = useState([]);
+  useEffect(() => {
+    axios.get(apiUrl + "/skills").then((res) => {
+      setSkills(res.data.data);
+    });
+  }, []);
+
+  const [experiences, setExperiences] = useState([]);
+  useEffect(() => {
+    axios.get(apiUrl + "/experience_options").then((res) => {
+      setExperiences(res.data);
+    });
+  }, []);
+
+  const [stances, setStances] = useState([]);
+  useEffect(() => {
+    axios.get(apiUrl + "/stance_options").then((res) => {
+      setStances(res.data);
+    });
+  }, []);
+
   return (
     <>
-    <Header backPath={"/user/:id"} back={"<"} forWardPath={""} forward={""} />
-    <div className='mx-auto max-w-[900px] p-8 my-8'>
-      {/* <h1>{ id }</h1> */}
-      <div className='flex justify-between'>
-        <img src={icon} alt="" style={{ width: '80px',height:"80px"}}/>
-        <button className='mr-20 bg-indigo-700 h-14 px-8 rounded-xl text-white font-bold'>更新する</button>
-      </div>
-      <div className="h-[500px] w-full">
-        <form action="" className="">
-          <label htmlFor="username" className="block">名前</label>
-          <input type="text" id="username" className="border-2 border-black h-10 max-w-full	w-[400px]" onChange={(e)=>setUserName(e.target.value)} value={userName}/>
-          <label htmlFor="input-profile" className="block">自己紹介</label>
-          <textarea name="input-profile" id="input-profile" className="border-2 border-black h-20 w-[400px]	max-w-full h-[250px] " onChange={(e)=>setUserProfile(e.target.value)} value={userProfile}></textarea>
-        </form>
-      </div>
-
-      <div className='mx-auto'>
-        <h2 className='text-xl font-bold mt-10'>技術</h2>
-        <div className='mt-4 flex flex-wrap'>
-          <Button text={"フロントエンド"} id={"1"} />
-          <Button text={"バックエンド"} id={"2"} />
-          <Button text={"インフラ"} id={"3"} />
+      <Header backPath={"/user/:id"} back={"<"} forWardPath={""} forward={""} />
+      <div className="mx-auto max-w-[900px] p-8 my-8">
+        {/* <h1>{ id }</h1> */}
+        <div className="flex justify-between">
+          <img src={icon} alt="" style={{ width: "80px", height: "80px" }} />
+          <button className="bg-indigo-700 py-4 px-6 h-fit rounded-lg text-white font-bold">
+            更新する
+          </button>
+        </div>
+        <div className="w-full">
+          <form action="" className="mt-4">
+            <label htmlFor="username" className="block mt-4 mb-2">
+              名前
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="border border-gray-700 p-2 max-w-full	w-[400px]"
+              onChange={(e) => setUserName(e.target.value)}
+              value={userName}
+            />
+            <label htmlFor="input-profile" className="block mt-4 mb-2">
+              自己紹介
+            </label>
+            <textarea
+              name="input-profile"
+              id="input-profile"
+              className="border border-gray-700 h-20 w-[400px] max-w-full resize-none p-2"
+              onChange={(e) => setUserProfile(e.target.value)}
+              value={userProfile}
+            ></textarea>
+            <div className="flex flex-col gap-8 mt-8">
+              <div>
+                <div className="mb-4 flex gap-4 items-end">
+                  <h2 className="text-2xl">技術</h2>
+                  <span className="text-gray-500">複数選択可</span>
+                </div>
+                <div className="flex flex-wrap gap-x-2 gap-y-2">
+                  {skills.map((skill) => {
+                    return (
+                      <CheckButton
+                        key={skill.id}
+                        type="checkbox"
+                        inputName="skills"
+                        value={skill.id}
+                        Name={skill.name}
+                      ></CheckButton>
+                    );
+                  })}
+                </div>
+              </div>
+              <div>
+                <div className="mb-4 flex gap-4 items-end">
+                  <h2 className="text-2xl">経験</h2>
+                  <span className="text-gray-500">単一選択</span>
+                </div>
+                <div className="flex flex-wrap gap-x-2 gap-y-2">
+                  {experiences.map((experience) => {
+                    return (
+                      <CheckButton
+                        key={experience.id}
+                        type="radio"
+                        inputName="experience"
+                        value={experience.id}
+                        Name={experience.name}
+                      ></CheckButton>
+                    );
+                  })}
+                </div>
+              </div>
+              <div>
+                <div className="mb-4 flex gap-4 items-end">
+                  <h2 className="text-2xl">スタンス</h2>
+                  <span className="text-gray-500">単一選択</span>
+                </div>
+                <div className="flex flex-wrap gap-x-2 gap-y-2">
+                  {stances.map((stance) => {
+                    return (
+                      <CheckButton
+                        key={stance.id}
+                        type="radio"
+                        inputName="stance"
+                        value={stance.id}
+                        Name={stance.name}
+                      ></CheckButton>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
-
-      <div>
-        <h2 className='text-xl font-bold mt-10'>ハッカソン出場経験</h2>
-        <div className='mt-4 flex flex-wrap'>
-          <Button text={"0回"} id={"4"} />
-          <Button text={"1回~3回"} id={"5"} />
-        </div>
-      </div>
-      <div>
-        <h2 className='text-xl font-bold mt-10'>ハッカソンへのスタンス</h2>
-        <div className='mt-4 flex flex-wrap'>
-          <Button text={"とりあえず楽しみたい"} id={"6"} />
-          <Button text={"入賞希望"} id={"7"} />
-          <Button text={"がんばる"} id={"8"} />
-
-        </div>
-      </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
 export default userIdEdit;
